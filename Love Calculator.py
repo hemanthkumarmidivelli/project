c41966b0755638aea5523a03490e520f28116438
hemanthkumarmidivelli <111556951+hemanthkumarmidivelli@users.noreply.github.com>
2023-04-10 20:53:03 +0530

    Update Love Calculator.py

--- Love Calculator.py
+++ Love Calculator.py
@@ -20,13 +20,26 @@ def ok(e1111):
     ans=messagebox.askokcancel("Love Calculator <3",(v),icon='info')
     if ans==0:
         root.destroy()
+def okk():
+    name=e.get()
+    lname=e2.get()
+    name=str(name)
+    lname=str(lname)
+    if name=="" or lname=="":
+        v="Name can not be empty"
+    elif name==lname:
+        v="Self love : 100%   <3"
+    else:
+        v="{} loves {} ".format(name,lname)+str(random.randrange(50,101))+"%     <3"
+    ans=messagebox.askokcancel("Love Calculator <3",(v),icon='info')
+    if ans==0:
+        root.destroy()
     
     
 root=Tk()
 root.title("Love calculator <3 by kittu")
-root.geometry("600x600")
+root.geometry("100x500")
 root.configure(bg="white")
-
 
 
 
@@ -40,6 +53,18 @@ l1.grid(column=1,row=2)
 e=Entry(root,width=27)
 e.grid(column=1,row=3)
 
+l2=Label(root,text="Your crush name",bg="white",fg="black")
+l2.grid(column=1,row=5)
+
+e2=Entry(root,width=27)
+e2.grid(column=1,row=6)
+e.focus()
+
+
+b=Button(root,command=okk,text="Calculate",bg="red",height=1,width=20)
+b.grid(column=1,row=8)
+root.bind('<Return>', ok)
+root.mainloop()
 l2=Label(root,text="Your crush name",bg="white",fg="black")
 l2.grid(column=1,row=5)
 
